13311240d047ccf2b37c8d5c83dcf6d5390763f3
Luke Bares <luke@nehemedia.de>
2019-01-17 23:10:50 +0100

    German translation

---
+++ src/translations/de/craft-flowplayer-drive.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Craft Flowplayer Drive plugin for Craft CMS 3.x
+ *
+ * This plugin includes Flowplayer Drive into craftcms.
+ *
+ * @link      http://luke.nehemedia.de
+ * @copyright Copyright (c) 2018 Lucas Bares
+ */
+
+/**
+ * Craft Flowplayer Drive de Translation
+ *
+ *
+ * @author    Lucas Bares
+ * @package   CraftFlowplayerDrive
+ * @since     1.0.0
+ */
+return [
+
+    // Settings
+    'Site-ID' => 'Site-ID',
+    'Site-ID-instructions' => 'Sie finden Ihre Site-ID in den <a href="https://flowplayer.com/app/workspace/settings" target="_blank">Workspace Einstellungen</a> in Flowplayer Drive.',
+    'API-Key' => 'API-Key',
+    'API-Key-instructions' => 'Sie finden Ihren API-Key in den <a href="https://flowplayer.com/app/workspace/settings" target="_blank">Workspace Einstellungen</a> in Flowplayer Drive.',
+    'User-ID' => 'User-ID',
+    'User-ID-instructions' => 'Sie finden Ihre User-ID in den <a href="https://flowplayer.com/app/user/account" target="_blank">Benutzeraccount Einstellungen</a> in Flowplayer Drive, direkt unter dem Profilbild. Die Benutzer-ID wird zum Hinzufügen und Löschen von Videos benötigt.',
+    'Default Player-ID' => 'Standard Player-ID',
+    'Default Player-ID-instructions' => 'Wenn Sie den integrierten Einbettungs-Code verwenden wollen, müssen Sie eine Player-ID angeben. Im Flowplayer Drive finden Sie eine <a href="https://flowplayer.com/app/players/" target="_blank">Liste von Playern</a>. Sie können die Player-ID entweder der URL, oder dem Einbettungscode entnehmen.',
+
+
+    // CP Labels
+    'Flowplayer Drive' => 'Flowplayer Drive',
+    'Video List'  => 'Videoliste',
+    'New Video'   => 'Neues Video',
+    'Create a new video' => 'Neues Video erstellen',
+    'Edit video' => 'Video bearbeiten',
+    'Refresh Videolist' => 'Videoliste aktualisieren',
+    'Clear Videolist' => 'Videoliste leeren',
+    'Refresh Index' => 'Index aktualisieren',
+
+    // Field
+    'FlowplayerVideoField' => 'Flowplayer Drive Video',
+    'Choose video'  => 'Video auswählen',
+    'Display only published videos' => 'Nur veröffentlichte Videos anzeigen',
+    'Private or unpublished videos will not be selectable' => 'Private, nicht mehr öffentliche Videos werden nicht angezeigt und können somit nicht ausgewählt werden.',
+    'Limit' => 'Begrenzung',
+    'Limit the number of selectable videos' => 'Grenzen Sie die Nummer der auswählbaren Videos ein.',
+
+    // Sources
+    'All Videos'    => 'Alle Videos',
+    'Public Videos'    => 'Öffentliche Videos',
+    'Private Videos'    => 'Private Videos',
+
+    // Statuses
+    'Published State'   =>  'Veröffentlicht',
+    'Private State'     =>  'Privat',
+
+    // Attributes
+    'Name'          =>  'Name',
+    'Created at'    =>  'Erstellt am',
+    'Published at'  =>  'Veröffentlicht am',
+    'Updated at'    =>  'Geändert am',
+    'Views'         =>  'Aufrufe',
+    'Published'     =>  'Veröffentlicht',
+    'Video-ID'      =>  'Video-ID',
+    'Entry-ID'      =>  'Eintrags-ID',
+    'Status'        =>  'Status',
+    'Description'   =>  'Beschreibung',
+
+
+    // Messages
+    'Are you sure you want to delete the selected videos?'   =>  'Wollen Sie die ausgewählten Videos wirklich löschen?',
+    'Videos successfully deleted.' => 'Videos erfolgreich gelöscht.',
+    'Video successfully saved.' => 'Video erfolgreich gespeichert.',
+
+    'Videolist refreshed. {updated} videos updated, {new} new videos indexed.' => '{updated} Videos wurden aktualisiert, {new} wurden neu hinzugefügt.',
+    'Videolist cleared' => 'Videliste geleert',
+
+    // Instructions
+
+
+];
